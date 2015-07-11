75d043d2ec8ae3bfc9e7b20f34e7e7b47a44335a
drenfr01 <duncanrenfrow@gmail.com>
2015-07-11 18:05:26 -0400

    save points

--- meteor/lib/collections/transactions.js
+++ meteor/lib/collections/transactions.js
@@ -2,18 +2,37 @@
 //put in maybe some CSS classes in here?
 Transactions = new Mongo.Collection('transactions');
 
+//create transaction with user1 and null user2
+//create a market collection as well
 Transactions.attachSchema({
-  userId1: {
+  user1Id: {
     type: String,
-    label: 'User A'
+    label: 'User 1'
   }, 
-  userId2: {
+  user1Items: {
+    type: [Object],
+    label: 'Items of User 1 in trade'
+  },
+  user2Id: {
     type: String,
     label: 'User B'
+  },
+  user2Items: {
+    type: [Object],
+    label: 'Items of User 2 in trade'
   },
   stage: {
     type: String,
     label: 'Current Stage',
-    allowedValues: ['INITIAL_OFFER'] //may need compound states, i.e. A accepts B rejects
+    //may need compound states, i.e. A accepts B rejects
+    allowedValues: ['INITIAL_OFFER', 'DECLINED', 'CANCELED', 'ACCEPTED'] 
   }
 });
+
+Transactions.initialize = function(user1Id, user1Items, user2Id, user2Items, stage) {
+  return Transactions.insert({user1Id: user1Id, 
+                             user1Items: user1Items,
+                             user2Id: user2Id,
+                             user2Items: user2Items,
+                             stage: stage});
+};
--- meteor/server/lib/API/transactionsAPI.js
+++ meteor/server/lib/API/transactionsAPI.js
@@ -1,19 +1,51 @@
 //basically all functions for transactions are here 
 //only thing that touches transactions collection
+
+//Stub API
+function takeItems(items) {
+  return true;
+}
+
+function giveItems(items) {
+  return true;
+}
+
+function returnItems(offerId) {
+  return true;
+}
+
+function queryOffer(offerId) {
+  return true;
+}
+function cancelOffer(offerId) {
+  return true;
+}
+
 TransactionsAPI = (function() {
 
-  var transaction; 
+  var transactionId;
+  var stage;
   
   var getCurrentStage = function() {
-
+    return stage;
   };
   
   return {
-    moveStage: function(transactionId, currentStage, possibleStages) {
-      return true;
+    //First method to call, sets up DB row, returns possible 
+    //stages
+    initializeTrade: function(user1Id, user1Items, user2Id, user2Items, stage) {
+      stage = "INITIAL_OFFER";
+      transactionId = Transactions.initialize(user1Id, user1Items, user2Id, user2Items, stage);
+      //send notification to player
     },
-    getNextPossibleStages: function(transactionId) {
-      return true;
+    acceptTrade: function() {
+      
+    }, 
+    declineTrade: function() {
+
+    },
+    rejectTrade: function() {
+
     }
   };
 })();
