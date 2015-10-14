df59af43431628da5498daa3ed75f735767bad33
Drew Proud <drew.proud@hotmail.com>
2015-10-14 18:51:34 -0400

    SAVE POINT

--- meteor/packages/steambot/SteamBot.js
+++ meteor/packages/steambot/SteamBot.js
@@ -118,7 +118,7 @@ SteamBot.prototype.getSteamId = function() {
 };
 
 SteamBot.prototype.loadBotInventory = function() {
-  // try {
+  try {
     var self = this;
 
     var Future = Npm.require('fibers/future');
@@ -142,9 +142,9 @@ SteamBot.prototype.loadBotInventory = function() {
     self.itemsUpdatedTimestamp = new Date();
 
     console.log('Bot inventory loaded!');
-  // } catch (e) {
-  //   console.log('Could not load bot inventory');
-  // }
+  } catch (e) {
+    console.log('Could not load bot inventory');
+  }
 };
 
 SteamBot.prototype.getItemObjsWithIds = function(partnerSteamId, items) {
