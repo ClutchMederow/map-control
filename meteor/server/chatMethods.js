c6d5fa5260e7989ca70569a64212d4f38f6a54e2
Drew Proud <drew.proud@hotmail.com>
2015-10-29 23:45:41 -0400

    fixed z-index bug

--- meteor/server/chatMethods.js
+++ meteor/server/chatMethods.js
@@ -1,4 +1,29 @@
 Meteor.methods({
+  test: function(attributes) {
+    check(attributes, {
+      text: String,
+      channel: String
+    });
+
+    this.userId = 'oj75Az7ifWnRxLGep';
+
+    var user = Users.findOne(this.userId);
+
+    //TODO: make this cleaner, risky to rely on names
+    var channel = Channels.findOne({ name: attributes.channel });
+    var items = Chat.parseChatText(attributes.text);
+
+    attributes.items = items;
+    attributes.channel = channel;
+    attributes.user = {userId: this.userId, profile: user.profile};
+
+    if (channel.category === 'Private') {
+      DB.insertPrivateChat(attributes);
+    } else {
+      DB.insertChat(attributes);
+    }
+  },
+
   insertChat: function(attributes) {
     check(attributes, {
       text: String,
