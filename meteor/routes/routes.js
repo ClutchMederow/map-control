188e631c54ff131b85e40975b8073d2567b58214
drenfr01 <duncanrenfrow@gmail.com>
2015-11-16 21:33:42 -0500

    adding in webhooks

--- meteor/routes/routes.js
+++ meteor/routes/routes.js
@@ -1,3 +1,24 @@
+WebApp.connectHandlers.use(function(req, res, next) {
+
+  // Check if request is for non-www address
+  if (req.headers && req.headers.host.slice(0, 4) !== 'www.') {
+
+    // Add www. from host
+    var newHost = 'www.' + req.headers.host;
+
+    // Redirect to www. version URL
+    res.writeHead(301, {
+      // Hard-coded protocol because req.protocol not available
+      Location: 'http://' + newHost + req.originalUrl
+    });
+    res.end();
+
+  } else {
+    // Continue with the application stack
+    next();
+  }
+});
+
 Router.configure({
   layoutTemplate: 'layout',
   notFoundTemplate: "notFound"
