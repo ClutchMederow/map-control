f6843708ab7f1c88cce89a07fe3b5c627cc7fe26
Drew Proud <drew.proud@hotmail.com>
2016-04-08 20:20:21 -0400

    Changed button to take you to inventory instead of home

--- meteor/client/views/user/manageStash.js
+++ meteor/client/views/user/manageStash.js
@@ -1,3 +1,7 @@
+/* global window */
+/* global sAlert */
+/* global StashManager */
+/* global Enums */
 
 Template.manageStash.helpers({
   stashTemplate: function() {
@@ -6,9 +10,9 @@ Template.manageStash.helpers({
 
   data: function() {
     return {
-      stashManager: Template.instance().stashManager
+      stashManager: Template.instance().stashManager,
     };
-  }
+  },
 });
 
 Template.manageStash.onCreated(function() {
@@ -17,9 +21,9 @@ Template.manageStash.onCreated(function() {
 });
 
 Template.manageStash.events({
-  'click #next': function(e, thisInstance) {
-    if (thisInstance.stashManager.hasItems()) {
-      thisInstance.stashTemplate.set('confirmStashTransaction');
+  'click #next': function(e, template) {
+    if (template.stashManager.hasItems()) {
+      template.stashTemplate.set('confirmStashTransaction');
       window.scrollTo(0,0);
     } else {
       sAlert.warning('Please select at least one item');
@@ -28,33 +32,33 @@ Template.manageStash.events({
     return true;
   },
 
-  'click #backToAddRemove': function(e, thisInstance) {
-    thisInstance.stashTemplate.set('addRemoveStash');
+  'click #backToAddRemove': function(e, template) {
+    template.stashTemplate.set('addRemoveStash');
     window.scrollTo(0,0);
   },
 
-  'click #submitStashTrans': function(e, thisInstance) {
-
-    if (thisInstance.stashManager.hasItems()) {
-      thisInstance.stashManager.execute();
-      thisInstance.stashTemplate.set('stashTransNextSteps');
+  'click #submitStashTrans': function(e, template) {
+    if (template.stashManager.hasItems()) {
+      template.stashManager.execute();
+      template.stashTemplate.set('stashTransNextSteps');
       window.scrollTo(0,0);
     } else {
       sAlert.warning('Please select at least one item');
     }
   },
 
-  'click #nextStepsHome': function() {
-    Router.go('home');
+  'click #nextStepsHome': function(event, template) {
+    event.preventDefault();
+    template.stashTemplate.set('addRemoveStash');
   },
 
-  'click #withdraw-div .contained-item': function(e, thisInstance) {
+  'click #withdraw-div .contained-item': function(e, template) {
     e.preventDefault();
-    thisInstance.stashManager.toggleItem(this, Enums.TransType.WITHDRAW);
+    template.stashManager.toggleItem(this, Enums.TransType.WITHDRAW);
   },
 
-  'click #deposit-div .contained-item': function(e, thisInstance) {
+  'click #deposit-div .contained-item': function(e, template) {
     e.preventDefault();
-    thisInstance.stashManager.toggleItem(this, Enums.TransType.DEPOSIT);
-  }
+    template.stashManager.toggleItem(this, Enums.TransType.DEPOSIT);
+  },
 });
